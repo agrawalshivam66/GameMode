829d181f8ba908ee0a7b8f65c440097c29c6a44c
Shivam Agrawal <agrawalshivam66@gmail.com>
2019-06-09 15:31:50 +0530

    last stable feedback options added and some minor changes

--- app/src/main/java/com/example/shivam_pc/gamemode/SettingActivity.java
+++ app/src/main/java/com/example/shivam_pc/gamemode/SettingActivity.java
@@ -2,17 +2,21 @@ package com.example.shivam_pc.gamemode;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.PersistableBundle;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -82,5 +86,17 @@ public class SettingActivity extends AppCompatActivity {
 
     }
 
-
+    // Email feedback for my personal address
+    public void feedback(View view) {
+        String addresses[] = {"agrawalshivam66@gmail.com"};
+        String subject = "Game Mode app feedback";
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+            Toast.makeText(this, "Please type your suggestions and attach screenshots if required", Toast.LENGTH_LONG).show();
+        }
+    }
 }
